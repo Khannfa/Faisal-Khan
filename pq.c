edde0a3812ec3750cdcda33d7cd66da6186347d1
FaisalAhmed Khan <khanfa@flip1.engr.oregonstate.edu>
2017-11-27 22:35:17 -0800

    Assignment 4 submitted

--- pq.c
+++ pq.c
@@ -4,27 +4,38 @@
  * you need in this file to implement a priority queue.  Make sure to add your
  * name and @oregonstate.edu email address below:
  *
- * Name:
- * Email:
+ * Name: Khanfa
+ * Email: Khanfa@oregonstate.edu
  */
 
 #include <stdlib.h>
-
+#include "dynarray.h"
 #include "pq.h"
 
 /*
  * This is the structure that represents a priority queue.  You must define
  * this struct to contain the data needed to implement a priority queue.
  */
-struct pq;
+struct pq{
+	struct dynarray *array;
 
+};
+	struct pq_elem{
+
+	int prior;
+	void* data;
+};
 
 /*
  * This function should allocate and initialize an empty priority queue and
  * return a pointer to it.
  */
 struct pq* pq_create() {
-  return NULL;
+	struct pq* pq = malloc(sizeof( struct pq*));
+	pq->array = dynarray_create();
+
+
+	return pq;
 }
 
 
@@ -37,6 +48,8 @@ struct pq* pq_create() {
  *   pq - the priority queue to be destroyed.  May not be NULL.
  */
 void pq_free(struct pq* pq) {
+	dynarray_free(pq->array);
+	free(pq);
 
 }
 
@@ -53,7 +66,16 @@ void pq_free(struct pq* pq) {
  *   Should return 1 if pq is empty and 0 otherwise.
  */
 int pq_isempty(struct pq* pq) {
-  return 1;
+
+
+	if(dynarray_size(pq->array) != 0){
+		int size = dynarray_size(pq->array);
+		return 0;
+	}
+	else if (dynarray_size(pq->array) == 0){
+		int size_2 = dynarray_size(pq->array);
+		return 1;
+	}
 }
 
 
@@ -74,8 +96,42 @@ int pq_isempty(struct pq* pq) {
  *     the element in the priority queue with the LOWEST priority value should
  *     be the FIRST one returned.
  */
-void pq_insert(struct pq* pq, void* value, int priority) {
+void pq_insert(struct pq* pq, void* data, int priority) {
+	struct pq_elem* e = malloc(sizeof(struct pq_elem));
+	struct pq_elem* node_parent = malloc(sizeof(struct pq_elem)); 
 
+	if(dynarray_size(pq->array) == 0)
+	{ 
+		e->data = data;
+		e->prior = priority;
+		dynarray_insert(pq->array,-1,e); 
+	}
+	else
+	{ 
+		e->data = data;
+		e->prior = priority; 
+		dynarray_insert(pq->array,-1,e);
+
+		int indx = dynarray_size(pq->array) - 1; 
+		int done = 0;
+		while(!done)
+		{ 
+			node_parent = dynarray_get(pq->array,(indx - 1) / 2);
+			if(node_parent->prior > e->prior && indx > 0)
+			{ 
+				dynarray_set(pq->array,(indx - 1) / 2, e);
+				dynarray_set(pq->array,indx,node_parent);
+				indx = (indx - 1) / 2; 
+			}
+			else
+			{ 
+				done = 1;
+			}
+
+		}
+	}
+//free(e);
+//free(node_parent);	
 }
 
 
@@ -92,7 +148,14 @@ void pq_insert(struct pq* pq, void* value, int priority) {
  *   LOWEST priority value.
  */
 void* pq_first(struct pq* pq) {
-  return NULL;
+  	if(dynarray_size(pq->array)==0){
+
+		return NULL;
+	}
+	else{
+		return ((struct pq_elem*)dynarray_get(pq->array,0))->data;
+
+	}
 }
 
 
@@ -109,8 +172,12 @@ void* pq_first(struct pq* pq) {
  *   with LOWEST priority value.
  */
 int pq_first_priority(struct pq* pq) {
-  return 0;
+  	struct pq_elem* e = malloc(sizeof(struct pq_elem));
+
+	e = dynarray_get(pq->array, 0);
+	return e->prior;
 }
+
 
 
 /*
@@ -127,5 +194,94 @@ int pq_first_priority(struct pq* pq) {
  *   LOWEST priority value.
  */
 void* pq_remove_first(struct pq* pq) {
-  return NULL;
+	struct pq_elem* e = NULL;
+	struct pq_elem* r = NULL;
+	struct pq_elem* lc = NULL;//leftchild
+	struct pq_elem* rc =NULL;//rightchild
+	int lci, rci;
+	r = dynarray_get(pq->array, 0);
+	if (dynarray_size(pq->array) == 1)
+	{
+		dynarray_remove(pq->array,-1);
+		return r->data;
+	}
+	dynarray_set(pq->array,0,dynarray_get(pq->array,-1));
+
+	dynarray_remove(pq->array,-1); 
+
+	int done = 0;
+	int indx = 0;
+	// can I group these as one percolation function?
+	while(!done)
+	{
+		lci = (indx * 2) + 1;//leftchildindex 
+		rci = (indx * 2) + 2;//rightchildindex
+
+
+		e = dynarray_get(pq->array, indx);
+
+		if(rci<dynarray_size(pq->array)&&lci<dynarray_size(pq->array)){
+
+
+			lc = dynarray_get(pq->array,lci);
+
+			rc = dynarray_get(pq->array,rci);
+
+			if(rc->prior >= lc->prior){		
+
+				if(e->prior> lc->prior){
+
+					dynarray_set(pq->array, lci, dynarray_get(pq->array, indx));
+
+					dynarray_set(pq->array, indx, lc);
+
+					indx = lci;				
+
+				}	
+				else{
+					done = 1;
+				}
+
+			}
+			else{
+
+
+				if(e->prior>rc->prior){
+
+					dynarray_set(pq->array, rci, dynarray_get(pq->array, indx));
+
+					dynarray_set(pq->array, indx, rc);
+
+					indx = rci;
+				}
+
+				else{
+					done = 1;
+				}
+
+			}
+		}
+		else if(lci<dynarray_size(pq->array)){
+			
+			lc = dynarray_get(pq->array,lci);
+			done =1;
+
+			if(lc->prior<e->prior){		
+
+
+				dynarray_set(pq->array, lci, dynarray_get(pq->array, indx));
+
+				dynarray_set(pq->array, indx, lc);
+
+			}
+		}
+
+		else{
+			done =1;
+		}
+	}
+
+	return r->data;
+
 }
+
